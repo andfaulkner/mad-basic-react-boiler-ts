505ab524f0e7375598982f69c238d9bb38946fab
Andrew Faulkner <andfaulkner@gmail.com>
2018-10-08 05:00:17 -0400

    Working CSS modules in Jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  roots: [`<rootDir>/app`],
-  transform: {
-    '^.+\\.tsx?$': `ts-jest`
-  },
-  testRegex: `(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$`,
-  moduleFileExtensions: [`ts`, `tsx`, `js`, `jsx`, `json`, `node`],
-  snapshotSerializers: [`enzyme-to-json/serializer`],
-  setupTestFrameworkScriptFile: `<rootDir>/config/setup-enzyme.ts`
+    roots: [`<rootDir>/app`],
+    transform: {
+        '^.+\\.tsx?$': `ts-jest`
+    },
+    testRegex: `(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$`,
+    moduleFileExtensions: [`ts`, `tsx`, `js`, `jsx`, `json`, `node`],
+    snapshotSerializers: [`enzyme-to-json/serializer`],
+    setupTestFrameworkScriptFile: `<rootDir>/config/setup-enzyme.ts`,
+    moduleNameMapper: {
+        '\\.(css|less)$': 'identity-obj-proxy'
+    }
 };
